a6ae4d658d30d2c81720147b9826283f1bff9668
onebitious <onebitious@icloud.com>
2017-04-11 20:32:13 +0900

    仕様とファイル名変更
    03_allFrameProp2gradientProp.jsx
    ↓
    03_frameProp2gradientProp.jsx
    選択している矩形を処理。
    選択している矩形がない場合、ドキュメント上の全ての矩形を処理する。
    ベタ塗りに対応。

--- 03_frameProp2gradientProp.jsx
+++ 03_frameProp2gradientProp.jsx
@@ -1,67 +1,85 @@
-﻿var doc = app.activeDocument; //ドキュメント
+﻿/*
+仕様
+選択している矩形から、グラデーションかベタ塗りかを取得。
+未選択の場合、ドキュメント上の全てのフレームを処理する。
+フレームは長方形ツールで作成されたPathItemに限る。
+グラデーションポイントを含めカラーモードはRGBであること。
+ベンダープレフィックスは -webkit- のみ付与する。
+
+*/
+var doc = app.activeDocument; //ドキュメント
 var obj = doc.pathItems; //ドキュメント上のオブジェクト
+var sel = doc.selection; //選択しているテキストフレーム
 
-    var docWidth = doc.width; //ドキュメントの幅
-    var docHeight = doc.height; //ドキュメントの高さ
+var docWidth = doc.width; //ドキュメントの幅
+var docHeight = doc.height; //ドキュメントの高さ
 
-    var divArray = []; //div用の配列
-    var posiXArray = []; //X座標用の配列
-    var posiYArray = []; //Y座標用の配列
-    var widArray = []; //幅用の配列
-    var heiArray = []; //高さの配列
-    var cssArray = []; //CSS用の配列
+var divArray = []; //div用の配列
+var posiXArray = []; //X座標用の配列
+var posiYArray = []; //Y座標用の配列
+var widArray = []; //幅用の配列
+var heiArray = []; //高さの配列
+var cssArray = []; //CSS用の配列
 
+if (sel.length == 0) {
+    var processObj = obj; //フレームが未選択の場合、全てのフレームを対象にする
+} else {
+    var processObj = sel; //選択されている場合、選択されているフレームを対象にする
+}
 
-for (var i = 0, objLen = obj.length; i < objLen; i++) {
+for (var i = 0, processObjLen = processObj.length; i < processObjLen; i++) {
 
     var graStoArr = []; //グラデーションポイント用の配列
     var graArray = []; //グラデーション用の配列
-    var posiX = round(obj[i].position[0], 3); //X座標
+    var posiX = round(processObj[i].position[0], 3); //X座標
     posiXArray.push(posiX);
     var minus = -1; //マイナス
-    var posiY = round(obj[i].position[1] * minus, 3); //Y座標　負の値になるので-1を乗算
+    var posiY = round(processObj[i].position[1] * minus, 3); //Y座標　負の値になるので-1を乗算
     posiYArray.push(posiY);
-    var wid = round(obj[i].width, 3); //幅
+    var wid = round(processObj[i].width, 3); //幅
     widArray.push(wid);
-    var hei = round(obj[i].height, 3); //高さ
+    var hei = round(processObj[i].height, 3); //高さ
     heiArray.push(hei);
 
     var _div = "_div" + zeroPad([i + 1]); //セレクタ作成
 
     /*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            グラデーション取得
+            塗り取得
      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
-    var graStoRedArr = []; //red配列
-    var graStoGreenArr = []; //green配列
-    var graStoBlueArr = []; //blue配列
+    if (processObj[i].fillColor.typename == "GradientColor") { //グラデーションの場合
+        var graStoRedArr = []; //red配列
+        var graStoGreenArr = []; //green配列
+        var graStoBlueArr = []; //blue配列
 
-    var graSto = obj[i].fillColor.gradient.gradientStops; //グラデーションポイント
-    var graDeg = round((obj[i].fillColor.angle), 1); //角度
+        var graSto = processObj[i].fillColor.gradient.gradientStops; //グラデーションポイント
+        var graDeg = round((processObj[i].fillColor.angle), 1); //角度
 
-    for (var k = 0, graStoLen = graSto.length; k < graStoLen; k++) {
-        var graStoRed = graSto[k].color.red;
-        var graStoGreen = graSto[k].color.green;
-        var graStoBlue = graSto[k].color.blue;
-        graStoRedArr.push(graStoRed);
-        graStoGreenArr.push(graStoGreen);
-        graStoBlueArr.push(graStoBlue);
-        var graStoRedResult = getColor(graStoRedArr[k]); //red
-        var graStoGreenResult = getColor(graStoGreenArr[k]); //green
-        var graStoBlueResult = getColor(graStoBlueArr[k]); //blue
-        var graStoColResult = "#" + graStoRedResult + graStoGreenResult + graStoBlueResult; //カラーHEX値
-        var myRampPoint = round((graSto[k].rampPoint), 2); //グラデーションスライダー
-        var popGraResult = graStoColResult + " " + myRampPoint + "%";
-        graStoArr.push(popGraResult);
+        for (var k = 0, graStoLen = graSto.length; k < graStoLen; k++) {
+            var graStoRed = graSto[k].color.red;
+            var graStoGreen = graSto[k].color.green;
+            var graStoBlue = graSto[k].color.blue;
+            graStoRedArr.push(graStoRed);
+            graStoGreenArr.push(graStoGreen);
+            graStoBlueArr.push(graStoBlue);
+            var graStoRedResult = getColor(graStoRedArr[k]); //red
+            var graStoGreenResult = getColor(graStoGreenArr[k]); //green
+            var graStoBlueResult = getColor(graStoBlueArr[k]); //blue
+            var graStoColResult = "#" + graStoRedResult + graStoGreenResult + graStoBlueResult; //カラーHEX値
+            var myRampPoint = round((graSto[k].rampPoint), 2); //グラデーションスライダー
+            var popGraResult = graStoColResult + " " + myRampPoint + "%";
+            graStoArr.push(popGraResult);
 
+        }
+
+        var fillResult = "background: -webkit-linear-gradient(" + graDeg + "deg," + graStoArr + ");";
+        //-webkit- のベンダープレフィックス付けないとグラデーションが反転する
+
+    } else if (processObj[i].fillColor.typename == "RGBColor") { //単色の場合
+        var redColor = getColor(processObj[i].fillColor.red);
+        var greenColor = getColor(processObj[i].fillColor.green);
+        var blueColor = getColor(processObj[i].fillColor.blue);
+        var fillResult = 'background-color: #' + redColor + greenColor + blueColor;
     }
-    //var graString = graStoArr.toString(); //div用配列を文字列に変換　※変数上書き
-    //var graArray = graString.replace(/,/g, ""); //文字列化したCSVのカンマを削除
-    //graArray.push(graString);
-
-    //alert(graArray);
-
-    var fillResult = "background: -webkit-linear-gradient(" + graDeg + "deg," + graStoArr + ");";
-    //ベンダープレフィックス付けないとグラデーションが反転する
 
     cssArray.push('.' + _div + '{left:' + posiXArray[i] + 'px;top:' + posiYArray[i] + 'px;width:' + widArray[i] + 'px;height:' + heiArray[i] + 'px;' + fillResult + '}');
 
