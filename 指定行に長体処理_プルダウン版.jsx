dcee1e5c1832070de6ff2ffd2faea514c7d87179
onebitious <onebitious@icloud.com>
2018-03-09 14:00:36 +0900

    ファイル追加
    「指定行に長体処理_プルダウン版.jsx」
    「指定行に長体処理_ボタン版.jsx」

---
+++ 指定行に長体処理_プルダウン版.jsx
@@ -0,0 +1,83 @@
+﻿//#target illustrator
+//#targetengine mainProcess
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    変数定義
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+var doc = app.activeDocument; //ドキュメント
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    入力ダイアログ表示
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+win = new Window("palette", "行数指定", [200, 150, 380, 310]);
+win.add("statictext", [10, 0, 200, 55], "文字を選択して\r\n行数を指定してください。");
+var linesValue = win.add("dropdownlist", [10, 60, 80, 75], ["1 行", "2 行", "3 行", "4 行", "5 行"]);
+linesValue.selection = 0;
+var okBtn = win.add("button", [100, 60, 170, 80], "OK");
+var unDoBtn = win.add("button", [100, 90, 170, 110], "取り消し");
+var reDoBtn = win.add("button", [100, 120, 170, 140], "やり直し");
+var cancelBtn = win.add("button", [10, 120, 90, 140], "キャンセル");
+okBtn.active = true;
+win.show(); //ウィンドウを表示
+win.center(); //センターに表示
+
+//▼OKボタン
+okBtn.addEventListener('click', function () {
+    var bridgeTalk = new BridgeTalk(); //BridgeTalkを使用
+    bridgeTalk.target = "illustrator";
+    bridgeTalk.body = "setLinesValue();"
+    bridgeTalk.send();
+}, false);
+
+//▼取り消し
+unDoBtn.addEventListener('click', function () {
+    var bridgeTalk = new BridgeTalk(); //BridgeTalkを使用
+    bridgeTalk.target = "illustrator";
+    bridgeTalk.body = "doUnDo();"
+    bridgeTalk.send();
+}, false);
+
+//▼やり直し
+reDoBtn.addEventListener('click', function () {
+    var bridgeTalk = new BridgeTalk(); //BridgeTalkを使用
+    bridgeTalk.target = "illustrator";
+    bridgeTalk.body = "doReDo();"
+    bridgeTalk.send();
+}, false);
+
+//▼キャンセルボタン
+cancelBtn.addEventListener('click', function () {
+    win.close();
+}, false);
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    関数
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+//▼行数を指定する関数
+function setLinesValue() {
+    var sel = doc.selection; //選択
+    if (linesValue.selection == 0) { //switch文にできなかった。。。
+        var linesLen = 1;
+    } else if (linesValue.selection == 1) {
+        var linesLen = 2;
+    } else if (linesValue.selection == 2) {
+        var linesLen = 3;
+    } else if (linesValue.selection == 3) {
+        var linesLen = 4;
+    } else if (linesValue.selection == 4) {
+        var linesLen = 5;
+    }
+    while (sel.lines.length > linesLen) { //指定した行数になるまで-1づつ長体をかける
+        sel.horizontalScale -= 1;
+    }
+}
+
+//▼取り消し関数
+function doUnDo() {
+    undo();
+}
+
+//▼やり直し関数
+function doReDo() {
+    redo();
+}
---
+++ 指定行に長体処理_ボタン版.jsx
@@ -0,0 +1,105 @@
+﻿#target illustrator-22
+#targetengine main
+
+/*//////////////////////////////////////////////////  変数定義  ////////////////////////////////////////////////////////////////////*/
+var doc = app.activeDocument; //ドキュメント
+
+/*/////////////////////////////////////////////// 入力ダイアログ表示 ////////////////////////////////////////////////////////*/
+win = new Window("palette", "指定行数に長体処理", [200, 150, 350, 300]);
+var para01Btn = win.add("button", [10, 10, 70, 30], "１行");
+var para02Btn = win.add("button", [10, 35, 70, 55], "２行");
+var para03Btn = win.add("button", [10, 60, 70, 80], "３行");
+var para04Btn = win.add("button", [75, 10, 135, 30], "４行");
+var para05Btn = win.add("button", [75, 35, 135, 55], "５行");
+var para06Btn = win.add("button", [75, 60, 135, 80], "６行");
+var unDoBtn = win.add("button", [10, 90, 70, 110], "取り消し");
+var reDoBtn = win.add("button", [75, 90, 135, 110], "やり直し");
+var cancelBtn = win.add("button", [10, 120, 135, 140], "キャンセル");
+
+win.show(); //ウィンドウを表示
+win.center(); //センターに表示  
+
+/*//////////////////////////////////////////////////////////// 関数 //////////////////////////////////////////////////////////////*/
+//▼1～6行のボタンが押されたとき
+para01Btn.onClick = function () { //1行
+    num = 1;
+    btProcess();
+};
+
+para02Btn.onClick = function () { //2
+    num = 2;
+    btProcess();
+};
+
+para03Btn.onClick = function () { //3行
+    num = 3;
+    btProcess();
+};
+
+para04Btn.onClick = function () { //4行
+    num = 4;
+    btProcess();
+};
+
+para05Btn.onClick = function () { //5行
+    num = 5;
+    btProcess();
+};
+
+para06Btn.onClick = function () { //6行
+    num = 6;
+    btProcess();
+};
+
+//▼取り消しボタンが押されたとき
+unDoBtn.onClick = function () {
+    var bridgeTalk = new BridgeTalk(); //BridgeTalkを使用
+    bridgeTalk.target = "illustrator-22";
+    bridgeTalk.body = "undo();"
+    bridgeTalk.send();
+};
+
+//▼やり直しボタンが押されたとき
+reDoBtn.onClick = function () {
+    var bridgeTalk = new BridgeTalk(); //BridgeTalkを使用
+    bridgeTalk.target = "illustrator-22";
+    bridgeTalk.body = "redo();"
+    bridgeTalk.send();
+};
+
+//▼キャンセルボタンが押されたとき
+cancelBtn.onClick = function () {
+    win.close();
+};
+
+//▼長体処理関数　BridgeTalk
+function btProcess() {
+    var bridgeTalk = new BridgeTalk(); //BridgeTalkを使用
+    bridgeTalk.target = "illustrator-22";
+    bridgeTalk.body = "setBtnValue(num);"
+    bridgeTalk.send();
+}
+
+//▼行数を指定する関数
+function setBtnValue(num) {
+    var sel=doc.selection;
+    var paraLen = doc.selection.paragraphs.length; //選択している段落数
+    //▼エラー処理
+    if (paraLen > 1) {
+        alert("選択している文字が段落に跨ってます。\r\nご確認ください。");
+        return;
+    }
+    while (sel.lines.length > num) { //指定した行数になるまで-1づつ長体をかける
+        sel.horizontalScale -= 0.1;
+    }
+}
+
+//▼取り消し関数
+function doUnDo() {
+    undo();
+}
+
+//▼やり直し関数
+function doReDo() {
+    redo();
+}
